bc26c7859698dacf7de6043283d6822915d374ba
Hibikine Kage <goodsmile314159@gmail.com>
2020-07-20 01:27:17 +0900

    a

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 import iro from "@jaames/iro";
+const setColor = (color) =>
+  document
+    .getElementById("root")
+    .setAttribute("style", `background-color: ${color}`);
 const colorPicker = new iro.ColorPicker("#picker", {
   width: 320,
   color: "#f00",
 });
 colorPicker.on("color:change", function (color) {
-  console.log("hgoe");
-  document
-    .getElementById("root")
-    .setAttribute("style", `background-color: ${color.hexString}`);
+  beforeColor = color.hexString;
+  setColor(color.hexString);
 });
+const flashButton = document.getElementById("flash");
+let beforeColor = "";
+let t = null;
+let is_flash_on = true;
+let is_black_on = false;
+flashButton.onclick = () => {
+  if (t !== null) {
+    clearTimeout(t);
+    t = null;
+    return;
+  }
+  is_flash_on = true;
+  t = setInterval(() => {
+    if (is_black_on) {
+      return;
+    }
+    if (is_flash_on) {
+      setColor(beforeColor);
+      is_flash_on = !is_flash_on;
+      return;
+    }
+    setColor("#000");
+    is_flash_on = !is_flash_on;
+  }, 100);
+};
+const blackButton = document.getElementById("black");
+blackButton.onclick = () => {
+  if (is_black_on) {
+    is_black_on = false;
+    setColor(beforeColor);
+    return;
+  }
+  is_black_on = true;
+  setColor("#000");
+};
